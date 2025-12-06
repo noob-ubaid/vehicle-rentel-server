a498cd2acb67116b47c080eecfdb64b48586b5c0
noob-ubaid <ubaidurrahman661@gmail.com>
2025-12-06 18:58:56 +0600

    completed creatuser route

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -26,13 +26,13 @@ const loginUser = async (email: string, password: string) => {
 };
 
 const createUser = async (payload: Record<string, unknown>) => {
-  const { name, email, password, role } = payload;
+  const { name, email, password, role, phone } = payload;
   const hashedPassword = await bcrypt.hash(password as string, 10);
   const result = await pool.query(
     `
-    INSERT INTO users(name,email,password,role) VALUES($1,$2,$3,$4) RETURNING * 
+    INSERT INTO users(name,email,password,role,phone) VALUES($1,$2,$3,$4,$5) RETURNING * 
     `,
-    [name, email, hashedPassword, role]
+    [name, email, hashedPassword, role, phone]
   );
   return result;
 };
