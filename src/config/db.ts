85a406123df9844a5300bf3ed0f5fb6664aa49e6
noob-ubaid <ubaidurrahman661@gmail.com>
2025-12-07 11:17:47 +0600

    added new tables for bookings

--- src/config/db.ts
+++ src/config/db.ts
@@ -27,6 +27,18 @@ const initDB = async () => {
       availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available','booked'))
     )
   `);
+  await pool.query(`
+     CREATE TABLE IF NOT EXISTS bookings (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL REFERENCES users(id),
+    vehicle_id INTEGER NOT NULL REFERENCES vehicles(id),
+    rent_start_date DATE NOT NULL,
+    rent_end_date DATE NOT NULL,
+    total_price NUMERIC(10,2) NOT NULL CHECK (total_price > 0),
+    status VARCHAR(20) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned')),
+    CONSTRAINT date_check CHECK (rent_end_date > rent_start_date)
+);
+    `);
 };
 
 export default initDB;
